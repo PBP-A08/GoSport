8898a0ae69e3136e4f4de1c055f76e63ba6e87a0
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2025-10-15 11:49:25 +0700

    Transaction display

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,53 @@
 from django.shortcuts import render
 
+import uuid
+
+class FakeTransaction:
+    def __init__(self, payment_status, shipping_status, amount, amount_paid):
+        self.id = uuid.uuid4()
+        self.payment_status = payment_status
+        self.shipping_status = shipping_status
+        self.amount = amount
+        self.amount_paid = amount_paid
+
 # Create your views here.
 def show_main(request):
 
-    context = {}
+    fake_transaction_data = [
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+        FakeTransaction("Lunas", "Menunggu Pengantar", 300000, 300000),
+    ]
+
+    context = {
+        'transactions': fake_transaction_data,
+    }
 
     return render(request, "index.html", context)
