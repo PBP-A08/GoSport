924c161c0babe0d61557c8c5844e0ece71fe10f9
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2025-10-22 14:42:27 +0700

    Payment routings

--- payment/urls.py
+++ payment/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from payment.views import show_main
+from payment.views import (
+    show_main,
+    pay,
+    complete_transaction,
+    delete_transaction_ajax,
+)
 
 app_name = 'payment'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('pay/<uuid:id>/', pay, name='pay_ajax'),
+    path('complete/<uuid:id>/', complete_transaction, name='complete_transaction_ajax'),
+    path('delete/<uuid:id>/', delete_transaction_ajax, name='delete_transaction_ajax'),
 ]
