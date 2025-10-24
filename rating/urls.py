905e9250a5a8f55a34898326baba70a9d333c85b
yufanjonni <yufanjonni3@gamil.com>
2025-10-24 17:59:02 +0700

    edit modal adding update and delete

--- rating/urls.py
+++ rating/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from rating.views import add_review_ajax,edit_review_ajax,delete_review_ajax,show_rating_review_ajax
+from rating.views import add_review_ajax,edit_review_ajax,delete_review_ajax,show_rating_review_ajax,helper_function
 
 app_name = 'rating'
 
 urlpatterns = [
     path('add-review-ajax/<uuid:id>/', add_review_ajax, name='add_review_ajax'),
     path('edit-review-ajax/<uuid:id>/', edit_review_ajax, name='edit_review_ajax'),
+    path('show-rating-review-ajax/<uuid:id>', show_rating_review_ajax, name='show_rating_review_ajax'),
+    path('delete-review-ajax/<uuid:id>', delete_review_ajax, name='delete_review_ajax'),
+    path('helper-fuction/<uuid:id>',helper_function, name='helper_function')
 ]
