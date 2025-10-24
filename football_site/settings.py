673132bda33c58374601da0c0374e188c13cac76
Carmella Geraldine Sutrisna <carmella.geraldine@ui.ac.id>
2025-10-24 15:38:39 +0700

    main ada double di settings

--- football_site/settings.py
+++ football_site/settings.py
@@ -50,11 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'main.apps.MainConfig',
+    'main.apps.MainConfig',
     'rating',
     'payment',
-    'main',
-    'main.apps.MainConfig',
     'cart',
 ]
 
