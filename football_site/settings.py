7ea5c74ff8faa15471655cca9e0617688722480e
Carmella Geraldine Sutrisna <carmella.geraldine@ui.ac.id>
2025-11-25 00:07:25 +0700

    benerin link

--- football_site/settings.py
+++ football_site/settings.py
@@ -177,10 +177,8 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:5000",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:5000",
+    "http://127.0.0.1:8000 ",
+    "http://localhost:8000",
     "https://sherin-khaira-football-site.pbp.cs.ui.ac.id",
 ]
 
